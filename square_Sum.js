bd87de690005fdecb3895bb109a52c4d66a0a395
SyedAbdulZee <syedabdulzeeshan307@gmail.com>
2022-06-13 00:01:30 +0530

    main

---
+++ square_Sum.js
@@ -0,0 +1,19 @@
+
+
+function squareSum(num){
+    var a = 0;
+    while (a*a < num){
+        var b=0;
+        while (b*b < num){
+            if (a*a + b*b == num){
+                if(a<=b){
+                    console.log(a + "," +b);
+                }
+            }
+            b+=1;
+        }
+        a+=1;
+    }   
+}
+
+squareSum(185);
